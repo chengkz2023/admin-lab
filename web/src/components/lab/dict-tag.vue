0e754c82580cd7371e9830606be84825cf6f1eec
chengkz <chengkz2022@gmail.com>
2026-04-05 16:37:42 +0800

    feat(reusable): add DictTag component with extend-based color mapping

---
+++ web/src/components/lab/dict-tag.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tag :type="tagType">{{ label }}</el-tag>
+</template>
+
+<script setup>
+  import { ref, watch } from 'vue'
+  import { useDictionaryStore } from '@/pinia/modules/dictionary'
+
+  defineOptions({ name: 'DictTag' })
+
+  const props = defineProps({
+    dictType: { type: String, required: true },
+    value: { required: true }
+  })
+
+  const VALID_TYPES = ['success', 'warning', 'danger', 'info', '']
+  const dictStore = useDictionaryStore()
+  const label = ref(String(props.value))
+  const tagType = ref('')
+
+  watch(
+    () => [props.dictType, props.value],
+    async ([type, val]) => {
+      if (!type) return
+      const items = await dictStore.getDictionary(type, 1)
+      const matched = (items || []).find((i) => String(i.value) === String(val))
+      if (matched) {
+        label.value = matched.label
+        tagType.value = VALID_TYPES.includes(matched.extend) ? matched.extend : ''
+      } else {
+        label.value = String(val)
+        tagType.value = ''
+      }
+    },
+    { immediate: true }
+  )
+</script>
